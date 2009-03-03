04e5b3fc22be638e3dd574a74713e2f9f3517865
Andrew O'Neil <andy@andyofniall.net>
2009-03-03 05:41:44 +0000

    Disable dynamic height for now, until i can get it working

--- code/IFramePage.php
+++ code/IFramePage.php
@@ -12,7 +12,7 @@ class IFramePage extends Page {
 		
 		$fields->removeFieldFromTab('Root.Content.Main', 'Content');
 		$fields->addFieldToTab('Root.Content.Main', new TextField('IFrameUrl', 'IFrame URL'));
-		$fields->addFieldToTab('Root.Content.Main', new CheckboxField('DynamicHeight', 'Dynamically resize the IFrame height (this doesn\'t work if IFrame URL is on a different domain)'));
+		//$fields->addFieldToTab('Root.Content.Main', new CheckboxField('DynamicHeight', 'Dynamically resize the IFrame height (this doesn\'t work if IFrame URL is on a different domain)'));
 		$fields->addFieldToTab('Root.Content.Main', new NumericField('FixedHeight', 'Fixed Height (in pixels)'));
 		$fields->addFieldToTab('Root.Content.Main', new HtmlEditorField('Content', 'Alternate Content'));
 		
@@ -22,11 +22,11 @@ class IFramePage extends Page {
 
 class IFramePage_Controller extends Page_Controller {
 	function Height() {
-		if($this->DynamicHeight) {
+		/*if($this->DynamicHeight) {
 			return 'class="iframeautosize"';
-		} else {
+		} else {*/
 			return 'style="height: ' . $this->FixedHeight . 'px;"';
-		}
+		//}
 	}
 }
 
