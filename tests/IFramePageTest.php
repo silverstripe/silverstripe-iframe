067e8ca43fc3be2af8a5dd1144a4282e8a1ae431
Ingo Schommer <ingo@silverstripe.com>
2014-06-07 23:06:46 +1200

    Allow forcing of host page protocol
    Avoid mixed content issues when host page is https://
    but iframe URL is specified as http://.

--- tests/IFramePageTest.php
+++ tests/IFramePageTest.php
@@ -33,4 +33,49 @@ class IFramePageTest extends SapphireTest {
 		$iframe->FixedWidth = '200';
 		$this->assertContains('width: 200px', $iframe->getStyle(), 'Fixed width is settable');
 	}
+
+	public function testForceProtocol() {
+		$origServer = $_SERVER;
+
+		$page = new IFramePage();
+		$page->URLSegment = 'iframe';
+		$page->IFrameURL = 'http://target.com';
+
+		Config::inst()->update('Director', 'alternate_protocol', 'http');
+		Config::inst()->update('Director', 'alternate_base_url', 'http://host.com');
+		$page->ForceProtocol = '';
+		$controller = new IFramePage_Controller($page);
+		$response = $controller->init();
+		$this->assertNull($response);
+
+		Config::inst()->update('Director', 'alternate_protocol', 'https');
+		Config::inst()->update('Director', 'alternate_base_url', 'https://host.com');
+		$page->ForceProtocol = '';
+		$controller = new IFramePage_Controller($page);
+		$response = $controller->init();
+		$this->assertNull($response);
+
+		Config::inst()->update('Director', 'alternate_protocol', 'http');
+		Config::inst()->update('Director', 'alternate_base_url', 'http://host.com');
+		$page->ForceProtocol = 'http://';
+		$controller = new IFramePage_Controller($page);
+		$response = $controller->init();
+		$this->assertNull($response);
+
+		Config::inst()->update('Director', 'alternate_protocol', 'http');
+		Config::inst()->update('Director', 'alternate_base_url', 'http://host.com');
+		$page->ForceProtocol = 'https://';
+		$controller = new IFramePage_Controller($page);
+		$response = $controller->init();
+		$this->assertEquals($response->getHeader('Location'), 'https://host.com/iframe/');
+
+		Config::inst()->update('Director', 'alternate_protocol', 'https');
+		Config::inst()->update('Director', 'alternate_base_url', 'https://host.com');
+		$page->ForceProtocol = 'http://';
+		$controller = new IFramePage_Controller($page);
+		$response = $controller->init();
+		$this->assertEquals($response->getHeader('Location'), 'http://host.com/iframe/');
+
+		$_SERVER = $origServer;
+	}
 }
