95101fcbc6505c1d351caa1e5573d8a0f8a51177
Elliot Sawyer <elliot@silverstripe.com>
2014-07-14 13:54:40 +1200

    Disallow javascript protocol in IFrameURL using a whitelist of allowed schemes. Includes unit testing and a docblock to IFramePage validate function to explain exceptions and return types

--- tests/IFramePageTest.php
+++ tests/IFramePageTest.php
@@ -33,4 +33,45 @@ class IFramePageTest extends SapphireTest {
 		$iframe->FixedWidth = '200';
 		$this->assertContains('width: 200px', $iframe->getStyle(), 'Fixed width is settable');
 	}
+
+	function testAllowedUrls() {
+		$iframe = new IFramePage();
+
+		$tests = array(
+			'allowed' => array(
+				'http://anything',
+				'https://anything',
+				'page',
+				'sub-page/link',
+				'page/link',
+				'page.html',
+				'page.htm',
+				'page.phpissoawesomewhywouldiuseanythingelse',
+				'//url.com/page',
+				'/root/page/link',
+				'http://intranet:8888',
+				'http://javascript:8080',
+				'http://username:password@hostname/path?arg=value#anchor'
+			),
+			'banned' => array(
+				'javascript:alert',
+				'tel:0210001234',
+				'ftp://url',
+				'ssh://1.2.3.4',
+				'ssh://url.com/page'
+			)
+		);
+
+		foreach($tests['allowed'] as $url) {
+			$iframe->IFrameURL = $url;
+			$iframe->write();
+			$this->assertContains($iframe->IFrameURL, $url);
+		}
+
+		foreach($tests['banned'] as $url) {
+			$iframe->IFrameURL = $url;
+			$this->setExpectedException('ValidationException');
+			$iframe->write();
+		}
+	}
 }
